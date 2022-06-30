401b61f4b98fb79093e0931f6703a48da92aa64b
rocalex <rocmaildev@gmail.com>
2022-07-01 01:39:01 +0800

    add unit tests

--- bridge/src/lib.rs
+++ bridge/src/lib.rs
@@ -1,5 +1,5 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::near_bindgen;
+use near_sdk::{near_bindgen, AccountId};
 
 use std::collections::HashMap;
 
@@ -15,13 +15,15 @@ pub struct XpBridge {
 
 #[near_bindgen]
 impl XpBridge {
-    #[payable]
-    pub fn initialize(&mut self, group_key: [u8; 32]) {
-        // let account_id = env::signer_account_id();
-        self.paused = false;
-        self.tx_fees = 0;
-        self.group_key = group_key;
-        self.action_cnt = 0;
+    #[init]
+    pub fn new(group_key: [u8; 32]) -> Self {
+        Self {
+            consumed_actions: HashMap::new(),
+            paused: false,
+            tx_fees: 0,
+            group_key,
+            action_cnt: 0,
+        }
     }
 
     #[payable]
@@ -65,5 +67,44 @@ impl XpBridge {
     #[payable]
     pub fn validate_unfreeze_nft(&mut self) {
         // TODO:
+    }
+
+    pub fn get_group_key(&self) -> [u8; 32] {
+        self.group_key
+    }
+}
+
+#[cfg(all(test, not(target_arch = "wasm32")))]
+mod tests {
+    use ed25519_dalek::Keypair;
+    use near_sdk::test_utils::VMContextBuilder;
+    use near_sdk::{testing_env, VMContext};
+    use rand_core::OsRng;
+    use std::collections::HashMap;
+
+    use super::*;
+
+    fn get_context(is_view: bool) -> VMContext {
+        VMContextBuilder::new()
+            .signer_account_id("alice".parse().unwrap())
+            .is_view(is_view)
+            .build()
+    }
+
+    #[test]
+    fn test_bridge() {
+        let context = get_context(false);
+        testing_env!(context);
+
+        let mut csprng = OsRng {};
+        let kp = Keypair::generate(&mut csprng);
+        let group_key: [u8; 32] = kp.public.to_bytes();
+
+        let contract = XpBridge::new(group_key);
+
+        let context = get_context(true);
+        testing_env!(context);
+
+        assert_eq!(group_key, contract.get_group_key());
     }
 }
