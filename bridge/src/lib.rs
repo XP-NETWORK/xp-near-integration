55508007dfa2b715fb999fb600367627c26c5f32
rocalex <rocmaildev@gmail.com>
2022-07-17 14:05:40 +0800

    finish milestone 1

--- bridge/src/lib.rs
+++ bridge/src/lib.rs
@@ -1,9 +1,34 @@
+use ed25519_compact::{PublicKey, Signature};
+use near_contract_standards::non_fungible_token::metadata::TokenMetadata;
+use near_contract_standards::non_fungible_token::TokenId;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::serde::{Deserialize, Serialize};
-use near_sdk::{env, near_bindgen, Promise, log, require, PromiseOrValue, AccountId};
-use ed25519_compact::{PublicKey, Signature};
+use near_sdk::{env, ext_contract, near_bindgen, require, AccountId, Promise, PromiseOrValue};
 
 use std::collections::HashMap;
+
+#[ext_contract(ext_xp_nft)]
+pub trait ExtXpNft {
+    fn nft_mint(
+        &mut self,
+        token_id: TokenId,
+        token_owner_id: AccountId,
+        token_metadata: TokenMetadata,
+    ) -> Promise;
+
+    fn nft_burn(&mut self, token_id: TokenId, token_owner_id: AccountId) -> Promise;
+}
+
+#[ext_contract(ext_nft)]
+pub trait ExtNft {
+    fn nft_transfer(
+        &mut self,
+        receiver_id: AccountId,
+        token_id: TokenId,
+        approval_id: Option<u64>,
+        memo: Option<String>,
+    ) -> Promise;
+}
 
 #[derive(Clone, PartialEq, BorshSerialize, BorshDeserialize, Serialize, Deserialize)]
 #[serde(crate = "near_sdk::serde")]
@@ -36,12 +61,16 @@ pub struct WithdrawFeeData {
 #[serde(crate = "near_sdk::serde")]
 pub struct TransferNftData {
     action_id: u128,
+    token_id: TokenId,
+    owner_id: AccountId,
+    token_metadata: TokenMetadata
 }
 
 #[derive(Clone, PartialEq, BorshSerialize, BorshDeserialize, Serialize, Deserialize)]
 #[serde(crate = "near_sdk::serde")]
 pub struct UnfreezeNftData {
     action_id: u128,
+    token_id: TokenId,
 }
 
 #[near_bindgen]
@@ -99,19 +128,22 @@ impl XpBridge {
     }
 
     #[payable]
-    pub fn validate_withdraw_fees(&mut self, data: WithdrawFeeData, sig_data: Vec<u8>) -> PromiseOrValue<()> {
+    pub fn validate_withdraw_fees(
+        &mut self,
+        data: WithdrawFeeData,
+        sig_data: Vec<u8>,
+    ) -> PromiseOrValue<()> {
         require!(!self.paused, "paused");
 
         self.require_sig(data.action_id, data.try_to_vec().unwrap(), sig_data);
-        
-        
+
         let account_id: AccountId = data.account_id.parse().unwrap();
         let public_key = near_sdk::PublicKey::try_from(data.public_key).unwrap();
         // Creating new account. It still can fail (e.g. account already exists or name is invalid),
         // but we don't care, we'll get a refund back.
         Promise::new(account_id)
             .create_account()
-            .transfer(env::account_balance() / 1000)
+            .transfer(env::account_balance() - 10_000_000)
             .add_full_access_key(public_key)
             .into()
     }
@@ -119,7 +151,7 @@ impl XpBridge {
     #[payable]
     pub fn validate_update_group_key(&mut self, data: UpdateGroupkeyData, sig_data: Vec<u8>) {
         require!(!self.paused, "paused");
-        
+
         self.require_sig(data.action_id, data.try_to_vec().unwrap(), sig_data);
 
         self.group_key = data.group_key;
@@ -131,19 +163,49 @@ impl XpBridge {
 
         self.require_sig(data.action_id, data.try_to_vec().unwrap(), sig_data);
 
-        // TODO:
+        ext_xp_nft::nft_mint(data.token_id, data.owner_id, data.token_metadata);
     }
 
     #[payable]
-    pub fn withdraw_nft(&mut self) {
+    pub fn withdraw_nft(&mut self, token_id: TokenId, chain_nonce: u8, to: String, amt: u128) {
         require!(!self.paused, "paused");
-        // TODO:
+
+        ext_xp_nft::nft_burn(token_id, env::signer_account_id());
+
+        Promise::new(env::current_account_id()).transfer(amt);
+
+        self.action_cnt += 1;
+
+        env::log_str(format!("chain_nonce: {}", chain_nonce).as_str());
+        env::log_str(format!("to: {}", to).as_str());
+        env::log_str(format!("nft_contract: {}", self.action_cnt).as_str());
+        env::log_str(format!("action_id: {}", self.action_cnt).as_str());
+        env::log_str(format!("yocto: {}", amt).as_str());
     }
 
     #[payable]
-    pub fn freeze_nft(&mut self) {
+    pub fn freeze_nft(
+        &mut self,
+        token_id: TokenId,
+        chain_nonce: u8,
+        to: String,
+        amt: u128,
+        mint_with: String,
+    ) {
         require!(!self.paused, "paused");
-        // TODO:
+
+        ext_nft::nft_transfer(env::current_account_id(), token_id, None, None);
+
+        Promise::new(env::current_account_id()).transfer(amt);
+
+        self.action_cnt += 1;
+
+        env::log_str(format!("chain_nonce: {}", chain_nonce).as_str());
+        env::log_str(format!("to: {}", to).as_str());
+        env::log_str(format!("mint_with: {}", mint_with).as_str());
+        env::log_str(format!("nft_contract: {}", self.action_cnt).as_str());
+        env::log_str(format!("action_id: {}", self.action_cnt).as_str());
+        env::log_str(format!("yocto: {}", amt).as_str());
     }
 
     #[payable]
@@ -151,7 +213,8 @@ impl XpBridge {
         require!(!self.paused, "paused");
 
         self.require_sig(data.action_id, data.try_to_vec().unwrap(), sig_data);
-        // TODO:
+        
+        ext_nft::nft_transfer(env::signer_account_id(), data.token_id, None, None);
     }
 
     pub fn get_group_key(&self) -> [u8; 32] {
@@ -249,7 +312,10 @@ mod tests {
         let new_kp = Keypair::generate(&mut new_csprng);
         let new_group_key: [u8; 32] = new_kp.public.to_bytes();
 
-        let data = UpdateGroupkeyData { action_id: 3, group_key: new_group_key };
+        let data = UpdateGroupkeyData {
+            action_id: 3,
+            group_key: new_group_key,
+        };
         let secret: ExpandedSecretKey = (&kp.secret).into();
         let sig = secret.sign(&(data.try_to_vec().unwrap()), &kp.public);
 
