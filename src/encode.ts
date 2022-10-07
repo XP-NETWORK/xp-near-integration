8f1d307552c97fed9da8c35276eb10aedcba2d11
imsk17 <imsumitkumar17@gmail.com>
2022-10-07 18:16:11 +0530

    fix encode

--- src/encode.ts
+++ src/encode.ts
@@ -4,7 +4,7 @@ import BN from "bn.js";
 export class WhitelistData {
     @field({ type: "u128" })
     actionId: BN;
-    @field({ type: "string" })
+    @field({ type: "String" })
     tokenContract: string;
 
     constructor(data: WhitelistData) {
@@ -45,27 +45,27 @@ export class UnpauseData {
 // }
 
 export class TokenMetadataData {
-    @field({ type: option("string") })
+    @field({ type: option("String") })
     title: string | undefined;
-    @field({ type: option("string") })
+    @field({ type: option("String") })
     description: string | undefined;
-    @field({ type: option("string") })
+    @field({ type: option("String") })
     media: string | undefined;
     @field({ type: option(vec("u8")) })
     mediaHash: Uint8Array | undefined;
     @field({ type: option("u64") })
     copies: number | undefined;
-    @field({ type: option("string") })
+    @field({ type: option("String") })
     issuedAt: string | undefined;
-    @field({ type: option("string") })
+    @field({ type: option("String") })
     expiresAt: string | undefined;
-    @field({ type: option("string") })
+    @field({ type: option("String") })
     startsAt: string | undefined;
-    @field({ type: option("string") })
+    @field({ type: option("String") })
     updatedAt: string | undefined;
-    @field({ type: option("string") })
+    @field({ type: option("String") })
     extra: string | undefined;
-    @field({ type: option("string") })
+    @field({ type: option("String") })
     reference: string | undefined;
     @field({ type: option(vec("u8")) })
     referenceHash: Uint8Array | undefined;
@@ -78,11 +78,11 @@ export class TokenMetadataData {
 export class TransferNftData {
     @field({ type: "u128" })
     actionId: BN;
-    @field({ type: "string" })
+    @field({ type: "String" })
     mintWith: string;
-    @field({ type: "string" })
+    @field({ type: "String" })
     tokenId: string;
-    @field({ type: "string" })
+    @field({ type: "String" })
     tokenOwnerId: string;
     @field({ type: TokenMetadataData })
     tokenMetadata: TokenMetadataData;
@@ -91,17 +91,3 @@ export class TransferNftData {
         Object.assign(this, data);
     }
 }
-
-// export class UnfreezeNftData {
-//     @field({ type: "u64" })
-//     actionId: BN;
-//     @field({ type: fixedArray("u8", 32) })
-//     receiver: number[];
-//     @field({ type: fixedArray("u8", 32) })
-//     mint: number[];
-// }
-
-// export class WithdrawFeesData {
-//     @field({ type: "u64" })
-//     actionId: BN;
-// }
