ddbd115ece712b753f442913085e68de1608a313
imsk17 <imsumitkumar17@gmail.com>
2022-10-09 16:12:14 +0530

    contract: bridge: events: emit type as well

--- contract/xpbridge/src/events.rs
+++ contract/xpbridge/src/events.rs
@@ -2,7 +2,8 @@ use near_contract_standards::non_fungible_token::{Token, TokenId};
 use near_sdk::{
     env,
     serde::{Deserialize, Serialize},
-    serde_json, AccountId,
+    serde_json::{self},
+    AccountId,
 };
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -19,14 +20,18 @@ pub struct TransferNftEvent {
 
 impl TransferNftEvent {
     fn to_json_string(&self) -> String {
+        let event = Event{event: self, event_type: "TransferUnique"};
         // Events cannot fail to serialize so fine to panic on error
-        serde_json::to_string(self)
+        serde_json::to_string(&event)
             .ok()
             .unwrap_or_else(|| env::abort())
     }
 
     fn to_json_event_string(&self) -> String {
-        format!("EVENT_JSON:{}", self.to_json_string())
+        format!(
+            "EVENT_JSON:{}",
+            self.to_json_string()
+        )
     }
 
     pub fn emit(self) {
@@ -45,16 +50,27 @@ pub struct UnfreezeNftEvent {
     pub contract: AccountId,
 }
 
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct Event< 's, T> {
+    pub event_type: &'s str ,
+    pub event: T,
+}
+
 impl UnfreezeNftEvent {
     fn to_json_string(&self) -> String {
+        let event = Event{event: self, event_type: "UnfreezeUnique"};
         // Events cannot fail to serialize so fine to panic on error
-        serde_json::to_string(self)
+        serde_json::to_string(&event)
             .ok()
             .unwrap_or_else(|| env::abort())
     }
 
     fn to_json_event_string(&self) -> String {
-        format!("EVENT_JSON:{}", self.to_json_string())
+        format!(
+            "EVENT_JSON:{}",
+            self.to_json_string()
+        )
     }
 
     pub fn emit(self) {
